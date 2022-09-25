742e90ed62c7f2d8619ecc8cf946f38cd427108e
Azhar Hussain <muhammad.hussain@publicissapient.com>
2022-09-26 04:38:06 +0500

    feat: questionnaire submission

---
+++ src/app/utils/constants.ts
@@ -0,0 +1,590 @@
+export const USERS = [
+  'DrShahid007',
+  'DrNouman',
+  'DrMuneeb',
+  'DrAliRahman',
+  'DrSaidAlamKhan',
+];
+
+export const PROVINCES = ['Khyber Pakhtunkhwa'];
+
+export const RAPID_ANTIGEN_TESTS_CONDUCTED = [
+  { id: 1, testName: 'Malaria', tests: 0, positive: 0 },
+  { id: 2, testName: 'Dengue', tests: 0, positive: 0 },
+  { id: 3, testName: 'Hepatitis A', tests: 0, positive: 0 },
+  { id: 4, testName: 'Hepatitis E', tests: 0, positive: 0 },
+  { id: 5, testName: 'Cholera', tests: 0, positive: 0 },
+];
+
+// export const HYPOTHISIS = {
+//   submittedBy: 'Dr. Shahid',
+//   reportingDate: '9/25/2022',
+//   province: "Khyber Pakhtunkhwa",
+//   districts: 'lahore',
+//   tehsil: 'liaquatpur',
+//   uc: '86,56,566c',
+//   healthFacilityName: 'Killa abdulih Name',
+//   healthFacilityType: 'Killa abdulih Type',
+//   healthFacilityCode: '20221',
+//   healthFacilityCamp: 'Musa zai',
+//   phoneNumber: '03458033800',
+//   organizationName: "WHO",
+//   catchmentPopulation: 2555,
+// };
+// export const HYPOTHISIS = {
+//   districts: 'lahore',
+//   tehsil: 'liaquatpur',
+//   uc: '86,56,566c',
+//   health_facility_type: 'Killa abdulih',
+//   health_facility_camp: 'hujra nasir camp1',
+//   catchment_population: 2555,
+// };
+
+// export const CASE = {
+//   lessThen5: {
+//     male: 1,
+//     female: 5,
+//   },
+//   greaterThen5: {
+//     male: 1,
+//     female: 5,
+//   },
+// };
+// export const DEATH = {
+//   lessThen5: {
+//     male: 1,
+//     female: 5,
+//   },
+//   greaterThen5: {
+//     male: 1,
+//     female: 5,
+//   },
+// };
+
+// export const QUESTIONS_CATEGORY1 = {
+//   lessThen5: {
+//     male: 0,
+//     female: 0,
+//   },
+//   greaterThen5: {
+//     male: 0,
+//     female: 0,
+//   },
+// };
+// export const QUESTIONS_CATEGORY2 = {
+//   lessThen5: {
+//     male: 0,
+//     female: 0,
+//   },
+//   greaterThen5: {
+//     male: 0,
+//     female: 0,
+//   },
+// };
+
+export const QUESTIONS = [
+  {
+    qId: 1,
+    q: 'Acute watery diarrhea (AWD) /Suspected Cholera',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 2,
+    q: 'Bloody Diarrhea/ Another acute diarrhea',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 3,
+    q: 'Acute Upper Respiratory Tract Infection',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 4,
+    q: 'Suspected Dengue Fever',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 5,
+    q: 'Pneumonia',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 6,
+    q: 'Suspected COVID-19',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 7,
+    q: 'Suspected Dengue Fever',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 8,
+    q: 'Suspected Malaria',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 9,
+    q: 'Suspected Chickenpox',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 10,
+    q: 'Suspected Typhoid Fever',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 11,
+    q: 'Neonatal tetanus/Neonatal sepsis',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 12,
+    q: 'Acute Jaundice Syndrome (Hepatitis-A and E)',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 14,
+    q: 'Suspected Hemorrhagic Fever/CCHF',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 15,
+    q: 'Unexplained Fever > 38.5˚C (PUO)',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 16,
+    q: 'Skin Disease/Scabies',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 17,
+    q: 'Cutaneous Leishmaniasis',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 18,
+    q: 'Acute Malnutrition',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 19,
+    q: 'Eye Infection',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 20,
+    q: 'Impetigo',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 21,
+    q: 'Dog Bite/Snake Bite',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+  {
+    qId: 22,
+    q: 'Others',
+    case: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+    death: {
+      lessThen5: {
+        male: 0,
+        female: 0,
+      },
+      greaterThen5: {
+        male: 0,
+        female: 0,
+      },
+    },
+  },
+];
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'emergency-disease-surveillance',
+    appId: '1:1052602535295:web:85a00cb5a103740a169e4a',
+    databaseURL: 'https://emergency-disease-surveillance-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'emergency-disease-surveillance.appspot.com',
+    locationId: 'asia-east2',
+    apiKey: 'AIzaSyC9BBKCuy2vnbaWjVhiCIvsku_swiS887k',
+    authDomain: 'emergency-disease-surveillance.firebaseapp.com',
+    messagingSenderId: '1052602535295',
+    measurementId: 'G-R9ZR5JE4QW',
+  },
   production: false
 };
 
